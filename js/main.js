628b8848de5bd61a7e13df1915588a2650920a1a
FeliciaPompei <f.pompei@hotmail.com>
2022-03-10 17:24:09 +0100

    js-array-object-carousel parte estetica

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@
  *
  *
  *
-Riprendiamo l'esercizio carosello e rifacciamolo, questa volta usando gli oggetti, prendendo come riferimento il codice scritto oggi insieme a lezione, che troverete direttamente nella mia repository di github a questo link: [link github]
-
-Modifichiamo il codice dell'esercizio per renderlo funzionante con un array di oggetti al posto dei tre array separati, con una sola regola: non è possibile modificare l'HTML ma solamente JS e CSS.
-Ricordiamo sempre l'importanza dell'integrità del dato.
 
 Bonus 1:
 Sperimentiamo attraverso l'uso delle timing functions anche una funzionalità di scorrimento al nostro carosello:
@@ -20,29 +16,29 @@ E se volessi un bottone per invertire la "direzione" del carosello?
 
 const carouselInfo = [
     {
-        image : 'img/01.jpg',
-        title : 'Svezia',
-        subtitle : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+        image : 'http://cdn.shopify.com/s/files/1/0560/8744/5712/products/EnTL8E7XUAEFyp7_1200x1200.jpg?v=1633707699',
+        title : 'Chicken were T-rexs',
+        subtitle : 'Chickens are the closest living relative to the T. rex, though perhaps not quite as scary!',
     },
     {
-        image : 'img/02.jpg',
-        title : 'Svizzera',
-        subtitle : 'Lorem ipsum',
+        image : 'https://odditymall.com/includes/content/you-can-now-get-arms-for-your-chickens-thumb.jpg',
+        title : 'Chicken play',
+        subtitle : 'Chickens love to play and will run, jump and sunbathe when given the chance\, yet billions of them spend their entire lives in cramped sheds with less floor space than an A4 piece of paper',
     },
     {
-        image : 'img/03.jpg',
-        title : 'Gran Bretagna',
-        subtitle : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+        image : 'https://i.ebayimg.com/images/g/BkIAAOSw8zZg4~NW/s-l400.jpg',
+        title : 'Chicken can dream',
+        subtitle : 'Chickens dream just like we do. They experience REM (rapid eye movement) during sleep, indicating that their minds may be wandering far from the four walls of the factory farms where they are imprisoned\.',
     },
     {
-        image : 'img/04.jpg',
-        title : 'Germania',
-        subtitle : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        image : 'https://i.etsystatic.com/26833745/r/il/c8bde9/3204005065/il_fullxfull.3204005065_7ssy.jpg',
+        title : 'Spaghetti grass',
+        subtitle : 'They slurp grass like spaghetti. Yes, you read that right!',
     },
     {
-        image : 'img/05.jpg',
-        title : 'Paradise',
-        subtitle : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam',
+        image : 'https://ae01.alicdn.com/kf/H5963fcbd32d14ca6a7adf7e6ad499cd7w/Chicken-Arm-Toy-Gag-Gift-Chicken-Arms-Props-Chicken-Forelimb-Decoration-Spoof-For-Pet-Themed-Party.jpg_Q90.jpg_.webp',
+        title : 'Chicken films',
+        subtitle : 'One of the best films made about chicken is \"Chicken Run \", it\'s about a hen called Ginger who has to save her friends and herself from the farm called tweedy\'s farm',
     },
     
 ]
